7e3a503fe95bda75afe2f17bb58b2dc09e33b481
tobykwmok <tobykwmok@gmail.com>
2026-03-02 14:41:43 +0000

    addewd brs prefix

--- anki_tree_generator.py
+++ anki_tree_generator.py
@@ -1,4 +1,5 @@
 res = []
+TITLE_NAME = 'BRS'
 
 with open('BRS 2a8f587eccc180619563c2ca6bcacfba.md', 'r') as file:
     lines = [x.strip() for x in file.readlines() if x.strip() != '']
@@ -15,7 +16,7 @@ with open('BRS 2a8f587eccc180619563c2ca6bcacfba.md', 'r') as file:
             print(current_header)
         elif line[0] == '[':
             page_name = line[1:line.index(']')]
-            res.append(f'{current_header}::{page_name}')
+            res.append(f'{TITLE_NAME}::{current_header}::{page_name}')
         
         i += 1
 
